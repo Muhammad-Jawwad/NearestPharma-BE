4d73cb51b5520a30caf41c3c8d7f2fe83c12444a
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2024-03-28 03:00:21 +0500

    few errors resolved

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const bodyParser = require("body-parser");
 const areaRoutes = require("./api/routes/area");
 const pharmacyRoutes = require("./api/routes/pharmacy");
@@ -11,16 +13,27 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// To protect from CORS
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
+
+// Apply rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later"
 });
+
+app.use(limiter);
+
+// To protect from CORS
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, PATCH, DELETE"
+//     );
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
 
 // Routes
 app.use("/area", areaRoutes);
